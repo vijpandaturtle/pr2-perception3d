ea4e1f6f2feedbdfa9f40d36fae044b3fa1d700d
Vijayasri Iyer <thisisvij98@gmail.com>
2017-10-04 16:34:16 +0530

    Code added
    Added todo's for exercise-3 in perception.py

--- pr2_robot/scripts/perception.py
+++ pr2_robot/scripts/perception.py
@@ -79,7 +79,7 @@ def pcl_callback(pcl_msg):
     # TODO: Extract inliers and outliers
     cloud_table = cloud_filtered.extract(inliers, negative=False)
     cloud_objects = cloud_filtered.extract(inliers, negative=True)
-    
+
     # TODO: Euclidean Clustering
     white_cloud = XYZRGB_to_XYZ(cloud_objects)
     tree = white_cloud.make_kdtree()
@@ -114,18 +114,40 @@ def pcl_callback(pcl_msg):
 # Exercise-3 TODOs:
 
     # Classify the clusters! (loop through each detected cluster one at a time)
+    detected_objects_list = []
+    detected_objects_labels = []
 
         # Grab the points for the cluster
+    for index, pts_list in enumerate(cluster_indices):
+        pcl_cluster = cloud_objects.extract(pts_list)
+        cluster_ros = pcl_to_ros(pcl_cluster)
+        chists = compute_color_histograms(sample_cloud, using_hsv=False)
+        normals = get_normals(sample_cloud)
+        nhists = compute_normal_histograms(normals)
 
         # Compute the associated feature vector
+        feature = np.concatenate((chists, nhists))
+        labeled_features.append([feature, model_name])
 
         # Make the prediction
+        prediction = clf.predict(scaler.transform(feature,reshape(-1,1)))
+        label = encoder.inverse_transform(prediction[0])
+        detected_objects_labels.append(label)
 
         # Publish a label into RViz
+        label_pos = list(white_cloud[pts_list[0]])
+        label_pos[2] += .4
+        object_markers_pub.publish(make_label(label, label_pos, index))
 
         # Add the detected object to the list of detected objects.
+        do = DetectedObject()
+        do.label = label
+        do.cloud = ros_cluster
+        detected_objects.append(do)
 
     # Publish the list of detected objects
+    rospy.loginfo('Detected {} objects : {}'.format(len(detected_objects_labels), detected_objects_labels))
+    detected_objects_pub.publish(detected_objects)
 
     # Suggested location for where to invoke your pr2_mover() function within pcl_callback()
     # Could add some logic to determine whether or not your object detections are robust
